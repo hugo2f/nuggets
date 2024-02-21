6b23be524f2a2bdd0393805aef3ffd4d46c1253a
Hugo Fang <hugo.fang.25@dartmouth.edu>
2024-02-21 16:04:57 -0500

    more function prototypes

--- gamemap/gamemap.c
+++ gamemap/gamemap.c
@@ -1,5 +1,5 @@
 /*
- * gamemap.c    Hugo Fang    2/21/2024
+ * GameMap_T.c    Hugo Fang    2/21/2024
  * 
  * defines the struct and functions related to storing and processing the game map
  */
@@ -10,19 +10,19 @@
 
 #include "file.h"
 
-typedef struct {
+typedef struct GameMap {
     int numRows, numCols; // size of map
     char** grid; // terrain features
     char** gameGrid; // also stores players and gold 
-} GameMap;
+} GameMap_T;
 
-GameMap* loadMapFile(char* mapFilePath)
+GameMap_T* loadMapFile(char* mapFilePath)
 {
   FILE* fp = fopen(mapFilePath, "r");
   if (fp == NULL) {
     return NULL;
   }
-  GameMap* map = malloc(sizeof(GameMap));
+  GameMap_T* map = malloc(sizeof(GameMap_T));
   if (map == NULL) {
     fclose(fp);
     return NULL;
@@ -69,7 +69,7 @@ GameMap* loadMapFile(char* mapFilePath)
   return map;
 }
 
-void deleteGameMap(GameMap* map)
+void deleteGameMap_T(GameMap_T* map)
 {
   if (map == NULL) {
     return;
@@ -94,7 +94,7 @@ void deleteGrid(char** grid, int numRows)
   free(grid);
 }
 
-char getCellType(GameMap* map, int row, int col)
+char getCellType(GameMap_T* map, int row, int col)
 {
   if (row < 0 || row >= map->numRows || col < 0 || col >= map->numCols) {
     return NULL;
@@ -108,16 +108,26 @@ char** getPlayerMap()
   return NULL; 
 }
 
-char** getVisibleRegion(GameMap* map, int row, int col)
+char** getVisibleRegion(GameMap_T* map, int row, int col)
 {
   // TODO
   return NULL;
 }
 
-char** getSpectatorMap(GameMap* map)
+char** getSpectatorMap(GameMap_T* map)
 {
   if (map == NULL) {
     return NULL;
   }
   return map->gameGrid;
+}
+
+void movePlayer(GameMap_T* map)
+{
+
+}
+
+void spawnGold(GameMap_T* map)
+{
+  
 }
