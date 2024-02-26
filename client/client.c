1806ba4e6f1e4e1191df788be8f3f6d6ca1c7f74
Joe Hirsh <josephhirsh9@gmail.com>
2024-02-25 21:26:34 -0500

    improved style, fixed Makefile, removed client object file

--- client/client.c
+++ client/client.c
@@ -2,29 +2,33 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <strings.h>
 #include <ncurses.h>
 #include "message.h"
 #include "graphics.h"
 
-typedef struct clientData {
+enum ClientState {
+    CLIENT_PRE_INIT,
+    CLIENT_INIT,
+    CLIENT_PLAY
+};
+
+typedef struct {
     char* playerName;
     char playerSymbol;
     int nrows;
     int ncols;
     int state;
-} clientData_t;
+} ClientData;
 
-static void parseArgs(const int argc, char* argv[], addr_t* serverp);
-static bool handleInputGeneric(void* server);
+static void parseArgs(int argc, char* argv[], addr_t* serverp);
+static bool handleInput(void* server);
 static bool handleInputSpecific(addr_t* serverp, const char validInputs[]);
 static bool handleMessage(void* arg, const addr_t from, const char* message);
-static void nextClientState();
+static void sendReceipt(addr_t* serverp);
 static bool sendStart(addr_t* serverp);
 static void sendPlay(addr_t* serverp);
-static void sendKey(addr_t* serverp, char message);
 static void sendSpectate(addr_t* serverp);
-static void sendReceipt(addr_t* serverp);
+static void sendKey(addr_t* serverp, char message);
 static void indicateInvalidKey(char key);
 static void handleOkay(char* symbol);
 static void handleGrid(char* coordinates);
@@ -33,22 +37,22 @@ static void handleDisplay(char* board);
 static void handleQuit(char* explanation);
 static void handleError(char* error);
 
-struct clientData client = {NULL, '\0', 0, 0, 0};
+ClientData client = {NULL, '\0', 0, 0, CLIENT_PRE_INIT};
 
 int 
-main(const int argc, char* argv[]) 
+main(int argc, char* argv[]) 
 {
     addr_t server;
     parseArgs(argc, argv, &server);
-    
-    bool messageLoopExitStatus = message_loop(&server, 0, NULL, handleInputGeneric, handleMessage);
-    
+
+    bool messageLoopExitStatus = message_loop(&server, 0, NULL, handleInput, handleMessage);
+
     message_done();
-    return messageLoopExitStatus? 0 : 1;
+    return messageLoopExitStatus ? EXIT_SUCCESS : 1;
 }
 
 void 
-parseArgs(const int argc, char* argv[], addr_t* serverp) 
+parseArgs(int argc, char* argv[], addr_t* serverp) 
 {
     const char* program = argv[0];
     if (argc != 3 && argc != 4) {
@@ -57,7 +61,7 @@ parseArgs(const int argc, char* argv[], addr_t* serverp)
     }
 
     if (message_init(NULL) == 0) {
-        fprintf(stderr, "Could not init message module!\n");
+        fprintf(stderr, "Could not initialize message module!\n");
         exit(3);
     }
 
@@ -74,61 +78,57 @@ parseArgs(const int argc, char* argv[], addr_t* serverp)
 }
 
 static bool 
-handleInputGeneric(void* server) 
+handleInput(void* server) 
 {
     addr_t* serverp = server;
-    if (serverp == NULL) {
-        fprintf(stderr, "InputHandler called with arg=NULL\n");
-        return true;
-    }
-    
-    if (!message_isAddr(*serverp)) {
-        fprintf(stderr, "InputHandler called without a correspondent\n");
+    if (serverp == NULL || !message_isAddr(*serverp)) {
+        fprintf(stderr, "Invalid server address\n");
         return true;
     }
 
-    if (client.state == 0) {
-        return sendStart(serverp);
-    } else if (client.state == 1) {
-        sendReceipt(serverp);
-        return false;
-    } else if (client.state == 2) {
-        if (client.playerName == NULL) {
-            return handleInputSpecific(serverp, "q");  
-        } else {
-            return handleInputSpecific(serverp, "qhljkyunb");  
-        }
-    } else {
-        fprintf(stderr, "Impossible client state");
-        return true;
+    switch (client.state) {
+        case CLIENT_PRE_INIT:
+            return sendStart(serverp);
+        case CLIENT_INIT:
+            sendReceipt(serverp);
+            return false;
+        case CLIENT_PLAY:
+            if (client.playerName == NULL) {
+                return handleInputSpecific(serverp, "q");
+            } else {
+                return handleInputSpecific(serverp, "qhljkyunb");
+            }
+        default:
+            fprintf(stderr, "Unknown client state\n");
+            return true;
     }
 }
 
 static bool 
-handleInputSpecific(addr_t* serverp, const char validInputs[])
-{    
-    char input = getch();
+handleInputSpecific(addr_t* serverp, const char validInputs[]) 
+{
+    int input = getch();
     if (strchr(validInputs, input) != NULL) {
         sendKey(serverp, input);
     } else {
         indicateInvalidKey(input);
     }
-    
+
     return false;
 }
 
-static bool
-handleMessage(void* arg, const addr_t from, const char* message)
+static bool 
+handleMessage(void* arg, const addr_t from, const char* message) 
 {
     if (message == NULL) return false;
-    
+
     char messageType[10];
     char remainder[100];
-    if (sscanf(message, "%s %99[^\n]", messageType, remainder) != 2) {
+    if (sscanf(message, "%9s %99[^\n]", messageType, remainder) != 2) {
         fprintf(stderr, "Received message with invalid format\n");
         return false;
     }
-    
+
     if (strcmp(messageType, "OK") == 0) {
         handleOkay(remainder);
     } else if (strcmp(messageType, "GRID") == 0) {
@@ -150,20 +150,15 @@ handleMessage(void* arg, const addr_t from, const char* message)
 }
 
 static void
-nextClientState() 
-{
-    if (client.state != 2) {
-        client.state += 1;
-    } else {
-        fprintf(stderr, "Attempting to transition to next client state amid game phase");
-    }
+sendReceipt(addr_t* serverp) {
+    message_send(*serverp, "received");
 }
 
 static bool 
 sendStart(addr_t* serverp) 
 {
-    if (client.state != 0) {
-        fprintf(stderr, "Sent START not during client pre-initialization phase");
+    if (client.state != CLIENT_PRE_INIT) {
+        fprintf(stderr, "Sent START not during client pre-initialization phase\n");
         return true;
     }
 
@@ -173,103 +168,97 @@ sendStart(addr_t* serverp)
         sendPlay(serverp);
     }
 
-    nextClientState();
+    client.state = CLIENT_INIT;
     return false;
 }
 
-static void
+static void 
 sendPlay(addr_t* serverp) 
 {
-    char message[100]; 
+    char message[100];
     sprintf(message, "PLAY %s", client.playerName);
     message_send(*serverp, message);
 }
 
-static void
+static void 
 sendSpectate(addr_t* serverp) 
 {
     message_send(*serverp, "SPECTATE");
 }
 
 static void 
-sendKey(addr_t* serverp, char key)
+sendKey(addr_t* serverp, char key) 
 {
-    if (client.state != 2) {
+    if (client.state != CLIENT_PLAY) {
         return;
     }
-    
-    char message[10]; 
+
+    char message[10];
     sprintf(message, "KEY %c", key);
     message_send(*serverp, message);
 }
 
-static void
-sendReceipt(addr_t* serverp) {
-    message_send(*serverp, "received");
-}
-
-static void
+static void 
 indicateInvalidKey(char key) 
 {
-    if (client.state != 3) {
+    if (client.state != CLIENT_PLAY) {
         return;
     }
-    
+
     char message[100];
     sprintf(message, "Invalid keystroke %c", key);
-    
+
     append_to_banner(message, client.ncols);
 }
 
 static void 
 handleOkay(char* symbol) 
 {
-    if (client.state != 1) {
+    if (client.state != CLIENT_INIT) {
         fprintf(stderr, "Received OK not during client initialization phase\n");
         return;
     }
 
-    char symbolCharacter = *symbol;
-    client.playerSymbol = symbolCharacter;
+    client.playerSymbol = *symbol;
 }
 
 static void 
 handleGrid(char* coordinates) 
 {
-    if (client.state != 1) {
+    if (client.state != CLIENT_INIT) {
         fprintf(stderr, "Received GRID not during client initialization phase\n");
         return;
     }
-    
+
     int nrows, ncols;
     if (sscanf(coordinates, "%d %d", &nrows, &ncols) != 2) {
         fprintf(stderr, "GRID message missing data\n");
         return;
     }
-    
+
     if (!init_curses(nrows, ncols)) {
         printf("You must enlarge the window to at least %d %d!\n", nrows, ncols);
         fflush(stdout);
     }
-    
+
     while (!init_curses(nrows, ncols));
-    
+
     client.nrows = nrows;
     client.ncols = ncols;
-        
-    display_banner(client.playerSymbol, 0, 0, 0); 
 
-    nextClientState();
+    display_banner(client.playerSymbol, 0, 0, 0);
+
+    client.state = CLIENT_PLAY;
 }
 
 static void 
 handleGold(char* counts) 
 {
-    if (client.state != 2) {
+    if (client.state != CLIENT_PLAY) {
         fprintf(stderr, "Received GOLD not during client phase\n");
         return;
     }
-    
+
     int collected, current, remaining;
     if (sscanf(counts, "%d %d %d", &collected, &current, &remaining) != 3) {
         fprintf(stderr, "GOLD message missing data\n");
@@ -279,27 +268,27 @@ handleGold(char* counts)
     char message[100];
     sprintf(message, "You collected %d nuggets!", collected);
 
-    display_banner(client.playerSymbol, current, remaining, message); 
+    display_banner(client.playerSymbol, current, remaining, message);
 }
 
 static void 
 handleDisplay(char* board) 
 {
-    if (client.state != 2) {
+    if (client.state != CLIENT_PLAY) {
         fprintf(stderr, "Received DISPLAY not during client phase\n");
         return;
     }
-    
+
     display_board(board, client.nrows, client.ncols);
 }
 
 static void 
 handleQuit(char* explanation) 
-{    
+{
     end_curses();
     printf("QUIT %s\n", explanation);
     fflush(stdout);
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
 
 static void 
