5de405a6dbac867a5edb07c2588013a75598c547
coho905 <coho905@gmail.com>
2024-02-28 12:29:24 -0500

    all errors resolved + makefile

--- user/user.h
+++ user/user.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <strings.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include "gamemap.h"
 
@@ -17,23 +18,23 @@
 typedef struct user user_t;
 
 
-user_t* user_new(char ID, GameMap* map, int gold, char* realName, int row, int col);
-user_t* getUserByID(user_t* user[], char ID);
-void user_move(user_t* user, char command);
+user_t* user_new(char ID, GameMap_t* map, int gold, char* realName, int row, int col);
+user_t* getUserByID(user_t* users[], char ID);
+void user_move(user_t* user, char command, GameMap_t* map, user_t* users[]);
 char getCharacterID(user_t* user);
 void stealGold(user_t* user1, user_t* user2);
 bool user_isActive(user_t* user);
 void user_inActive(user_t* user);
 int user_getGold(user_t* user);
 void user_delete(user_t* user);
-void moveDownRight(user_t* user, GameMap* map, user_t* user[]);
-void moveDownLeft(user_t* user, GameMap* map, user_t* user[]);
-void moveUpRight(user_t* user, GameMap* map, user_t* user[]);
-void moveUpLeft(user_t* user, GameMap* map, user_t* user[]);
-void moveUp(user_t* user, GameMap* map, user_t* user[]);
-void moveDown(user_t* user, GameMap* map, user_t* user[]);
-void moveLeft(user_t* user, GameMap* map, user_t* user[]);
-void moveRight(user_t* user, GameMap* map, user_t* user[]);
+void moveDownRight(user_t* user, GameMap_t* map, user_t* users[]);
+void moveDownLeft(user_t* user, GameMap_t* map, user_t* users[]);
+void moveUpRight(user_t* user, GameMap_t* map, user_t* users[]);
+void moveUpLeft(user_t* user, GameMap_t* map, user_t* users[]);
+void moveUp(user_t* user, GameMap_t* map, user_t* users[]);
+void moveDown(user_t* user, GameMap_t* map, user_t* users[]);
+void moveLeft(user_t* user, GameMap_t* map, user_t* users[]);
+void moveRight(user_t* user, GameMap_t* map, user_t* users[]);
 
 
 
