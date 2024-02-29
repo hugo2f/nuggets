31e470c859237f39103dad12ab130c93392df3b4
Joe Hirsh <josephhirsh9@gmail.com>
2024-02-29 18:08:53 -0500

    some comments

--- client/handlers.c
+++ client/handlers.c
@@ -7,69 +7,94 @@
 #include "graphics.h"
 #include "validators.h"
 
-
+/*
+ * Runs upon receiving message from server with the OK header; see .h for more details.
+ */
 void 
 handle_okay(char* symbol) 
 {
-    int errors = 0;
+    int errors = 0; // stores number of errors so function can accumulate multiple errors before exiting
+    
+    // errors if client attempts to handle OK before it sends START 
     if (client.state != CLIENT_START_SENT) {
         fprintf(stderr, "Received OK again or prior to sending START\n");
         errors++;
     }
 
+    // logs warning if the symbol received from server is more than one character
     if (strlen(symbol) > 1) {
         fprintf(stderr, "Received player symbol with multiple characters, attempting to use first\n");
     }
 
+    // grabs the first character off the symbol string (which ideally would already be one character)
     char symbolCharacter = *symbol;
     
+    // validates the symbol (ensures that it is alphabetic and captitalized)
     if (!validate_player_symbol(symbolCharacter)) {
         fprintf(stderr, "Received invalid player symbol\n");
         errors++;
     }
 
+    // ceases execution if error encountered
     if (errors != 0) {
         return;
     }
 
+    // otherwise, sets the client symbol
     client.playerSymbol = symbolCharacter;
 
+    // and advances to the next client state 
     client.state = CLIENT_OKAY_RECEIVED;
 }
 
+/*
+ * Runs upon receiving message from server with the GRID header; see .h for more details.
+ */
 void 
 handle_grid(char* coordinates) 
 {
-    int errors = 0;
+    int errors = 0; // stores number of errors so function can accumulate multiple errors before exiting
+    
+    // errors if client attempts to handle GRID before it handles OK 
     if (client.state != CLIENT_OKAY_RECEIVED) {
         fprintf(stderr, "Received GRID prior to receiving OK\n");
         errors++;
     }
 
-    int nrows, ncols;
+    int nrows, ncols; // nrows = number of rows of game map, ncols = number of columns of game map
+    
+    // extracts values from the coordinates string, and ensures that two values were extracted, erroring if not
     if (sscanf(coordinates, "%d %d", &nrows, &ncols) != 2) {
         fprintf(stderr, "GRID message missing data\n");
         errors++;
     }
 
+    // ceases execution if error encountered
     if (errors != 0) {
         return;
     }
 
+    // otherwise, it initializes curses
     init_curses(nrows, ncols);
 
+    // sets client nrows and client ncols which are global and used elswhere 
     client.nrows = nrows;
     client.ncols = ncols;
 
+    // depending on the client mode (player or spectator), display banner
     if (client.playerName != NULL) {
         display_player_banner(client.playerSymbol, 0, 0);
     } else {
         display_spectator_banner();
     }
 
+    // advance game state
     client.state = CLIENT_GRID_RECEIVED;
 }
 
+/*
+ * Runs upon receiving message from server with the GOLD header; see .h for more details.
+ */
 void 
 handle_gold(char* counts) 
 {
@@ -108,6 +133,9 @@ handle_gold(char* counts)
     indicate_nuggets_collected(collected);
 }
 
+/*
+ * Runs upon receiving message from server with the DISPLAY header; see .h for more details.
+ */
 void 
 handle_display(char* map) 
 {
@@ -123,6 +151,9 @@ handle_display(char* map)
     }
 }
 
+/*
+ * Runs upon receiving message from server with the QUIT header; see .h for more details.
+ */
 void 
 handle_quit(char* explanation) 
 {
@@ -132,6 +163,9 @@ handle_quit(char* explanation)
     exit(EXIT_SUCCESS);
 }
 
+/*
+ * Runs upon receiving message from server with the ERROR header; see .h for more details.
+ */
 void 
 handle_error(char* error) 
 {
